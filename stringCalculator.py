994ecbe6ec03d5641808d2f636828c2f97f8685d
Asif <asif2000nm@gmail.com>
2025-02-24 23:05:00 +0530

    step 2

--- stringCalculator.py
+++ stringCalculator.py
@@ -1,7 +1,5 @@
-def add(nums):
-    if len(nums) == 0:
+def add(numsString):
+    if numsString == "":
         return 0
-    if len(nums) == 1:
-        return int(nums)
-    if len(nums) == 3:
-        return int(nums[0]) + int(nums[-1])
+    nums = numsString.split(",")
+    return sum(map(int, nums))
--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestString(unittest.TestCase):
         self.assertEqual(add(""), 0)
         self.assertEqual(add("1"), 1)
         self.assertEqual(add("1,5"), 6)
-        
+        self.assertEqual(add("1,2,3,4,5,6,7,8,9,10"), 55)
 
 if __name__ == '__main__':
     unittest.main()
